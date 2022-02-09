7634c7e2d70f5d5e6e1fe407ca3ca40a72b88952
Taufiksk2 <99322664+Taufiksk2@users.noreply.github.com>
2022-02-09 22:15:57 +0700

    Add files via upload

---
+++ rumus tugas 1.py
@@ -0,0 +1,16 @@
+# Rumus tabung
+phi = 22/7
+r = 14
+t = 35
+
+# luas selimut
+luas_1 = 2 * phi * r * t
+print(luas_1)
+
+# luas permukaan
+luas_2 = ( 2 * phi * r * t ) + (2 * phi * r * r)
+print(luas_2)
+
+# Volume tabung 
+volume = phi * r * r * t
+print(volume)
